f171220798e5c03f3951e87782f66d17772e0ccd
Mrlinkerrorsystem <54057540+ahmadchen@users.noreply.github.com>
2020-04-15 00:27:24 +0700

    Hanyalah Tools Mini IP Penetration 🏡
    Hanyalah Tools Thanks h4ckertarget.com 😇🙊

---
+++ Ipentst/config.php
@@ -0,0 +1,36 @@
+<?php
+
+// Color
+$blue           = "\033[1;34m";
+$cyan           = "\033[1;36m";
+$okegreen       = "\033[92m";
+$lightgreen     = "\033[1;32m";
+$white          = "\033[1;37m";
+$purple         = "\033[1;35m";
+$red            = "\033[1;31m";
+$yellow         = "\033[1;33m";
+
+// API Link
+$mtr                    = "https://api.hackertarget.com/mtr/?q=";
+$nping                  = "https://api.hackertarget.com/nping/?q=";
+$dnslookup              = "https://api.hackertarget.com/dnslookup/?q=";
+$reversedns             = "https://api.hackertarget.com/reversedns/?q=";
+$hostsearch             = "https://api.hackertarget.com/hostsearch/?q=";
+$findshareddns          = "https://api.hackertarget.com/findshareddns/?q=";
+$zonetransfer           = "https://api.hackertarget.com/zonetransfer/?q=";
+$whois                  = "https://api.hackertarget.com/whois/?q=";
+$geoip                  = "https://api.hackertarget.com/geoip/?q=";
+$reverseiplookup        = "https://api.hackertarget.com/reverseiplookup/?q=";
+$nmap                   = "https://api.hackertarget.com/nmap/?q=";
+$subnetcalc             = "https://api.hackertarget.com/subnetcalc/?q=";
+$aslookup               = "https://api.hackertarget.com/aslookup/?q=";
+$bannerlookup           = "https://api.hackertarget.com/bannerlookup/?q=";
+$httpheaders            = "https://api.hackertarget.com/httpheaders/?q=";
+$pagelinks              = "https://api.hackertarget.com/pagelinks/?q=";
+$analyticslookup        = "https://api.hackertarget.com/analyticslookup/?q=";
+
+// Etc
+$user = trim(shell_exec('whoami'));
+$host = trim(shell_exec('hostname'));
+
+?>
